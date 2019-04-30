0a9da08d323c2e926ea1e1cb569026e03ab0a0c1
srujanchinta8 <42657821+srujanchinta8@users.noreply.github.com>
2019-04-29 20:48:10 -0400

    Adding code for multiple sensitive attributes

---
+++ src/edge_editing_ms.py
@@ -0,0 +1,106 @@
+from __future__ import division
+import networkx as nx
+import collections
+import random
+import string
+import matplotlib.pyplot as plt
+from operator import itemgetter
+from operator import attrgetter
+from matplotlib import pyplot as plt
+import time
+from src import helpers
+import time
+import os
+import psutil
+
+
+def edge_editing(k,l,n,I,cond,path):
+    '''
+
+    '''
+
+    mem_before = helpers.get_process_memory()
+    G = nx.read_gml(path)
+    G_old = nx.read_gml(path)
+    #Debgging
+    # print(1)
+    labels = []
+    i = 0
+    for node in G.nodes:
+        G.add_node(node, label = (node[0],node[0]), degree = G.degree[node], id = i)
+        i += 1
+        labels.append(G.node[node]['label'])
+        print("LOLOLOL",labels)
+    #Debugging
+    # print(2)
+    start_time = time.time()
+    initial_apl = 1
+    if cond == 1:
+        #Debugging
+        #print(1)
+        initial_apl = helpers.calculate_apl(G)
+        #Debugging
+        #print(initial_apl)
+    '''
+    # original_cc = nx.average_clustering(G)
+    # while p < number_runs:
+    #G = nx.convert_node_labels_to_integers(H, label_attribute = 'author_name')
+    #cloc = {}
+    #cloc = nx.closeness_centrality(G)
+    #cloc = sorted(cloc.items(), key = itemgetter(1), reverse = True)
+    #btwc = {}
+    #btwc = nx.betweenness_centrality(G)
+    #btwc = sorted(btwc.items(), key = itemgetter(1), reverse = True)
+    #eigc = {}
+    #eigc = nx.eigenvector_centrality(G)
+    #eigc = sorted(eigc.items(), key = itemgetter(1), reverse = True)
+    #for node in G.nodes:
+    #    G.add_node(node, clo_centrality = cloc[node], btw_centrality = btwc[node], eig_centrality = eigc[node])
+    #print G.nodes(data = True)
+                                            ## Taking input for K-Anonymity
+    #alpha = input("Enter the value of alpha \n")                                   ## Taking input for Alpha
+    #cent_measure = raw_input("Enter the centrality measure : \n")
+    '''
+    if n == 1:
+        G = helpers.generate_tds_onlyk(G,k,I)
+    elif n == 2:
+        G = helpers.generate_tds_kl(G,k,l,I)
+    elif n == 3:
+        G = helpers.generate_tds(G,k,l,I,3,0.25)
+    edge_target = []
+
+    for node in G.nodes():
+        edge_target.append(G.node[node]['target_degree'])
+        #Debugging
+        # print(G.node[node])
+    if sum(edge_target) % 2 != 0:
+        edge_target[0] += 1
+    G_new = nx.configuration_model(edge_target)
+    i = 0
+    for node in G_new.nodes:
+        G_new.add_node(node, label = labels[i])
+        i += 1
+    new_apl = 0
+    if cond == 1:
+        #Debuggging
+        # print(1)
+        new_apl = helpers.calculate_apl(G_new)
+        #print(new_apl)
+    #Debugging
+    # for node in G_new.nodes():
+        # print (G_new.degree[node])
+    #actual_centrality = []
+    '''
+    for i in helpers.centrality_top_20(G_old):
+        actual_centrality.append(G.node[i[0]]['id'])
+    print("New apl",new_apl)
+    print("Original apl",initial_apl)
+    print("Top 20 centrality in original graph", actual_centrality)
+    print("Top 20 centrality in new graph", helpers.centrality_top_20(G_new))
+    '''
+    end_time = time.time()
+    mem_after = helpers.get_process_memory()
+    x = end_time - start_time
+    y = mem_after - mem_before
+    error = (abs(new_apl-initial_apl)/initial_apl)*100
+    return G_old, G_new , x, y,error
---
+++ src/helpers_ms.py
@@ -0,0 +1,335 @@
+from __future__ import division
+import networkx as nx
+import collections
+import random
+import string
+from operator import itemgetter
+from matplotlib import pyplot as plt
+import time
+import os
+import psutil
+
+mem = []
+def calculate_apl(G):
+    add = 0
+    lol = 0
+    max_order = 2
+
+    #Checking to see if the graph is connected or not
+    if nx.is_connected(G):
+        return nx.average_shortest_path_length(G)
+
+    else:
+        for g in nx.connected_component_subgraphs(G):
+            if g.order() > 2:
+                add = add + nx.average_shortest_path_length(g)
+                lol = lol + 1
+                if g.order() > max_order:
+                    max_order = g.order()
+
+    return add/lol
+
+def centralitity(G,i):
+
+    degc = {}
+
+    if i == 1:
+        #print("Making use of eigenvector centrality")
+        degc = nx.eigenvector_centrality(G)
+    elif i == 2:
+        #print("Making use of closeness centrality")
+        degc = nx.closeness_centrality(G)
+    elif i == 3:
+        #print("Making use of betweenness centrality")
+        degc = nx.betweenness_centrality(G)
+    elif i == 4:
+        #print("Making use of degree centrality")
+        degc = nx.degree_centrality(G)
+    else:
+        #print("Making use of katz centrality")
+        degc = nx.katz_centrality_numpy(G)
+
+    return degc
+
+
+def plot_graph(G):
+    plt.figure()
+    pos_nodes = nx.spring_layout(G)
+    nx.draw(G, pos_nodes, with_labels=True)
+
+    pos_attrs = {}
+    for node, coords in pos_nodes.items():
+        pos_attrs[node] = (coords[0], coords[1] + 0.08)
+
+    node_attrs = nx.get_node_attributes(G, 'label')
+    custom_node_attrs = {}
+    for node, attr in node_attrs.items():
+        custom_node_attrs[node] = "{'label': '" + attr + "'}"
+
+    nx.draw_networkx_labels(G, pos_attrs, labels=custom_node_attrs)
+    plt.show()
+
+def get_mem():
+    return mem
+
+def get_title(i,j):
+
+    I = ['Only k','K & l', 'All ']
+    J = ['Eigen','closeness','betweeness','degree','katz']
+    s = 'Using ' + I[i] + ' and ' + J[j] + ' centrality'
+    return s
+
+def elapsed_since(start):
+    return time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
+
+def get_process_memory():
+    process = psutil.Process(os.getpid())
+    return process.memory_info().rss
+
+def profile(func):
+    def wrapper(*args, **kwargs):
+        mem_before = get_process_memory()
+        result = func(*args, **kwargs)
+        mem_after = get_process_memory()
+        mem.append(mem_after - mem_before)
+        return result
+    return wrapper
+def generate_tds(G,k,l,i,c_var = 3, alpha = 0.25):
+
+    #print("Using all for privacy measures")
+    k_val = []
+    k_val.append(k)
+    degc = {}
+    degc = centralitity(G,i)
+    degc = sorted(degc.items(), key = itemgetter(1), reverse = True)
+    target = G.node[degc[0][0]]['degree']                                      ## For the first time, setting target degree
+    labels_present = []
+    targets_present = []
+    temp_labels_present = []
+    count = 1                                                                  ## Count denotes number of nodes in each equivalence group
+    #Debugging
+    # print("Hello")
+    for val in degc:
+        if count > k:                                                          ## Adding K-Anonymity
+            c = collections.Counter(labels_present)
+            target_label_count = c.most_common(1)[0][1]                        ## count of the most frequent label
+            #Debugging
+            # print target_label_count / (count - 1), count                      ## debugging
+            temp_count = 0
+            recursive_sum = 0
+            dict(c)
+            for key,valuee in c.items():                                            ## For recursive c-l diversity
+                key = 0
+                if temp_count >= l:
+                    recursive_sum = recursive_sum + valuee
+                    temp_count = temp_count + key + 1
+                else:
+                    temp_count = temp_count + 1
+            if  target_label_count/(count-1) < alpha and len(temp_labels_present) >= l and target_label_count < (c_var*recursive_sum):                           ## Alpha Anonymity condition, count - 1 is used because the count is always 1 ahead of the actual number of nodes
+                target = G.node[val[0]]['degree']
+                count = 1
+                del labels_present[:]
+                del temp_labels_present[:]
+        G.add_node(val[0], target_degree = target)
+        labels_present.append(G.node[val[0]]['label'])                        ## All the labels present in the equivalence group
+        if G.node[val[0]]['target_degree'] not in targets_present:
+            targets_present.append(G.node[val[0]]['target_degree'])
+        if G.node[val[0]]['label'] not in temp_labels_present:
+            temp_labels_present.append(G.node[val[0]]['label'])               ## No of distinct labels present in the equivalence group
+        count = count + 1
+    return G
+
+def generate_tds_onlyk(G,k,i):        #Not Tested
+    #print("Using only k privacy measures")
+    degc = {}
+    #Debugging
+    # print(i)
+    degc = centralitity(G,i)
+    degc = sorted(degc.items(), key = itemgetter(1), reverse = True)
+    target = G.node[degc[0][0]]['degree']                                      ## For the first time, setting target degree
+    count = 1                                                                  ## Count denotes number of nodes in each equivalence group
+    #Debugging
+    # print("Hello")
+    for val in degc:
+        if count > k:                                                          ## Adding K-Anonymity
+            target = G.node[val[0]]['degree']
+            count = 1
+        G.add_node(val[0], target_degree = target)
+        count = count + 1
+    return G
+
+def generate_tds_kl(G,k,l,i):         #Not Tested
+    #print("Using k,l privacy measures")
+    k_val = []
+    k_val.append(k)
+    degc = {}
+    degc = centralitity(G,i)
+    degc = sorted(degc.items(), key = itemgetter(1), reverse = True)
+    target = G.node[degc[0][0]]['degree']                                      ## For the first time, setting target degree
+    labels_present = []
+    targets_present = []
+    temp_labels_present = []
+    count = 1                                                                  ## Count denotes number of nodes in each equivalence group
+    #Debugging
+    # print("Hello")
+    for val in degc:
+        if count > k:                                                          ## Adding K-Anonymity
+            c = collections.Counter(labels_present)
+            #Debugging
+            #target_label_count = c.most_common(1)[0][1]                        ## count of the most frequent label
+            #print target_label_count / (count - 1), count
+            dict(c)
+            if  len(temp_labels_present) >= l:
+                target = G.node[val[0]]['degree']
+                count = 1
+                del labels_present[:]
+                del temp_labels_present[:]
+        G.add_node(val[0], target_degree = target)
+        labels_present.append(G.node[val[0]]['label'])                        ## All the labels present in the equivalence group
+        if G.node[val[0]]['target_degree'] not in targets_present:
+            targets_present.append(G.node[val[0]]['target_degree'])
+        if G.node[val[0]]['label'] not in temp_labels_present:
+            temp_labels_present.append(G.node[val[0]]['label'])               ## No of distinct labels present in the equivalence group
+        count = count + 1
+    return G
+
+def centrality_top_20(G):
+    degc = nx.degree_centrality(G)
+    degc = sorted(degc.items(), key = itemgetter(1), reverse = True)
+    return degc[:20]
+
+def centrality_top_20_compare(G_old,G_new):
+    old,new = [],[]
+    I = 0
+    for node in G_old.nodes:
+        G_old.add_node(node, id = I,label = (node[0],node[0]))
+        I += 1
+
+    I = 0
+    for node in G_new.nodes:
+        G_new.add_node(node, id = I)
+        I += 1
+    #Debugging
+    # print("Hello")
+    for i in centrality_top_20(G_old):
+        old.append(G_old.node[i[0]]['id'])
+    for i in centrality_top_20(G_new):
+        print(i)
+        new.append(G_new.node[i[0]]['id'])
+        # new.append(i[0])
+    #Debugging
+    #print(old,new)
+    old,new = set(old),set(new)
+    res = old.intersection(new)
+    return len(res)
+
+def centrality_values(G):
+    original_centrality_val = []
+    noise_centrality_val = []
+    res = 0
+    cnt = 0
+    res_original_nodes = 0
+    cnt_original_nodes = 0
+    color_map = []
+    degc = {}
+    degc = nx.katz_centrality_numpy(G)
+    for n in G.nodes():
+        if G.node[n]['target_degree'] == 0 and G.node[n]['degree'] >=1:
+            color_map.append('blue')
+            res = res + degc[n]
+            cnt = cnt + 1
+            #Debugging
+            # print G.degree(n), G.node[n]['degree'], G.node[n]
+        else:
+            color_map.append('red')
+            res_original_nodes = res_original_nodes + degc[n]
+            cnt_original_nodes = cnt_original_nodes + 1
+
+    original_centrality_val.append(res_original_nodes / cnt_original_nodes)
+    noise_centrality_val.append(res / cnt)
+    plt.xlabel('Values of K')
+    plt.ylabel('Social Importance')
+    plt.legend()
+    plt.show()
+
+def plot_graph2(G1,G2, Title="Default Title"):
+
+    fig = plt.figure(figsize=(40,40))
+    fig.suptitle(Title, fontsize=20, fontweight='bold')
+
+    ax1 = fig.add_subplot(121)
+    ax1.set_title("Original Graph")
+
+    pos_nodes = nx.spring_layout(G1)
+    nx.draw(G1, pos_nodes, with_labels=True)
+
+    pos_attrs = {}
+    for node, coords in pos_nodes.items():
+        pos_attrs[node] = (coords[0], coords[1] + 0.08)
+
+    node_attrs = nx.get_node_attributes(G1, 'label')
+    custom_node_attrs = {}
+    print(len(node_attrs))
+    for node, attr in node_attrs.items():
+        custom_node_attrs[node] = "{label: " + attr[0] +","+attr[1]+ "}"
+
+    nx.draw_networkx_labels(G1, pos_attrs, labels=custom_node_attrs, ax=ax1, with_labels = True)
+
+
+    ###########
+
+    ax2 = fig.add_subplot(122)
+    ax2.set_title("Modified Graph")
+
+    pos_nodes = nx.spring_layout(G2)
+    nx.draw(G2, pos_nodes, with_labels=True)
+
+    pos_attrs = {}
+    for node, coords in pos_nodes.items():
+        pos_attrs[node] = (coords[0], coords[1] + 0.08)
+
+    node_attrs = nx.get_node_attributes(G2, 'label')
+    custom_node_attrs = {}
+    for node, attr in node_attrs.items():
+        print(attr)
+        custom_node_attrs[node] = "{label: " + attr[0] + ","+attr[1]+"}"
+
+    nx.draw_networkx_labels(G2, pos_attrs, labels=custom_node_attrs, ax=ax2)
+
+    plt.show()
+
+def plot_graph_summary(G1,Gs, Titles=[]):
+
+    fig = plt.figure(figsize=(40,40))
+    fig.suptitle("SUMMARY", fontsize=15, fontweight='bold')
+
+    for i in range(15):
+        row = i//5
+        col = i%5
+
+        if Titles:
+            title=Titles.pop(0)
+        else:
+            title="Default Title"
+
+        ax = plt.subplot2grid((3,5),(row,col))
+
+        ax.set_title(title, fontsize=10)
+
+        g = Gs[i]
+
+        pos_nodes = nx.spring_layout(g)
+        nx.draw(g, pos_nodes, with_labels=True)
+
+        pos_attrs = {}
+        for node, coords in pos_nodes.items():
+            pos_attrs[node] = (coords[0], coords[1] + 0.08)
+
+        node_attrs = nx.get_node_attributes(g, 'label')
+        custom_node_attrs = {}
+        for node, attr in node_attrs.items():
+            custom_node_attrs[node] = "{'label': '" + attr + "'}"
+
+        nx.draw_networkx_labels(g, pos_attrs, labels=custom_node_attrs, ax=ax)
+
+    plt.show()
